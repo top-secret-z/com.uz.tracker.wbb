878f627e5c5163271e160ae7f3358e5496939766
Sascha Greuel <github@1-2.dev>
2022-09-09 13:20:50 +0200

    5.5.0

---
+++ install.sql
@@ -0,0 +1,2 @@
+ALTER TABLE wcf1_user_tracker ADD wlwbbPost TINYINT(1) NOT NULL DEFAULT 1;
+ALTER TABLE wcf1_user_tracker ADD wlwbbThread TINYINT(1) NOT NULL DEFAULT 1;
---
+++ update_page.sql
@@ -0,0 +1,3 @@
+INSERT INTO	wcf1_user_tracker_page (class, page, isPublic) VALUES ('wbb\\page\\BoardPage', 'board', 1);
+INSERT INTO	wcf1_user_tracker_page (class, page, isPublic) VALUES ('wbb\\page\\ThreadPage', 'thread', 1);
+INSERT INTO	wcf1_user_tracker_page (class, page, isPublic) VALUES ('wbb\\page\\BoardFeedPage', 'rssFeedBoard', 1);
